c67d5cda6410024edbecb12c50014c906b6f9984
He Li <hli25@nd.edu>
2021-09-06 20:50:43 -0400

    add quick start files for testing models

---
+++ run_Laikago_pace.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 motion_imitation/run.py --mode test --motion_file motion_imitation/data/motions/Laikago/dog_pace.txt --model_file motion_imitation/data/policies/Laikago/dog_pace.zip --visualize
