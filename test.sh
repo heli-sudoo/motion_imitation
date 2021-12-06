bcc2b2d22276afff3a5d42ee56c939f10b33da12
vincekurtz <vjkurtz@gmail.com>
2021-12-06 15:32:59 -0500

    update bash to take arguments

--- test.sh
+++ test.sh
@@ -1,6 +1,16 @@
-# #!/bin/bash
-Mode=rollout
-Robot=A1
-Gait=trot
-Policy=trot02
-python3 motion_imitation/run.py --mode $Mode --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$Gait.txt --model_file motion_imitation/data/policies/$Robot/$Policy.zip --visualize
+#!/bin/bash
+if [ $# -eq 0 ]; then
+   echo "Usage: test.sh mode robot gait policy"
+   echo "options:"
+   echo "         mode: test rollout"
+   echo "         robot: A1 MC Laikago"
+   echo "         gait: trot pace"
+   echo "         policy: gait_num"
+else
+   Mode=$1
+   Robot=$2
+   Gait=$3
+   Policy=$4
+   python3 motion_imitation/run.py --mode $Mode --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$Gait.txt --model_file motion_imitation/data/policies/$Robot/$Policy.zip --visualize
+fi
+
