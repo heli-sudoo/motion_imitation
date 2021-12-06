aa5b2160087be46a8a1bca653175e74b1c2d6e4f
He Li <hli25@nd.edu>
2021-12-06 11:09:07 -0500

    trivial change

--- motion_imitation/examples/mini_cheetah/standing_jointPD.py
+++ motion_imitation/examples/mini_cheetah/standing_jointPD.py
@@ -11,8 +11,8 @@ import time
 from tqdm import tqdm
 from motion_imitation.robots import mini_cheetah as mc
 from motion_imitation.robots import robot_config
+from motion_imitation.envs.utilities import terrain
 
-GROUND_URDF_FILE = "plane.urdf"
 DES_JNT_ANGLES = np.array([0., -.8, 1.6] *4)
 def main():    
     # create a pybullet client and connect to GUI server
@@ -20,17 +20,24 @@ def main():
     # add pybullet_data to client search path
     pybullet_client.setAdditionalSearchPath(pd.getDataPath())
     pybullet_client.setGravity(0,0,-10)
-    pybullet_client.loadURDF(GROUND_URDF_FILE)
-    # pybullet_client.configureDebugVisualizer(p.COV_ENABLE_SINGLE_STEP_RENDERING,10)        
-
+    # pybullet_client.loadURDF("plane.urdf")
+    terrain.get_uneven_terrain()
+        
     robot = mc.MiniCheetah(pybullet_client=pybullet_client, action_repeat=1)
     # udpate the observation
     robot.ReceiveObservation()
     init_jnt_angles = robot.GetTrueMotorAngles() # in nparray
+    
+    robot_visuals = p.getVisualShapeData(robot.quadruped)
+    print(robot_visuals)
+    # change visual color of the robot
+    for visual_obj in robot_visuals:
+        if visual_obj[1] not in [3,7,11,15]:
+            p.changeVisualShape(robot.quadruped, visual_obj[1], rgbaColor = [0.65, 0.65, 0.65, 1])
 
     len_sim = 5000
     for t in tqdm(range(len_sim)):
-        print(t)
+        # print(t)
         progress = min(t/300, 1)
         des_jnt_angles = (1-progress)*init_jnt_angles + progress * DES_JNT_ANGLES
         action = des_jnt_angles
--- train.sh
+++ train.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 Robot=A1
 gait=trot
-num=1
-python3 motion_imitation/run.py --mode train --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$gait.txt --output_dir motion_imitation/data/policies/$Robot/$gait_$num --int_save_freq 10000000 --visualize
+num=01
+mkdir motion_imitation/data/policies/$Robot/$gait$num
+python3 motion_imitation/run.py --mode train --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$gait.txt --output_dir motion_imitation/data/policies/$Robot/$gait$num --int_save_freq 10000000 --visualize
