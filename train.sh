42157da9bff9b5e46c83ddcafdb85b3aab2f2653
vincekurtz <vjkurtz@gmail.com>
2021-12-15 17:57:51 -0500

    minor change

--- train.sh
+++ train.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 if [ $# -eq 0 ]; then
-   echo "Usage: train.sh robot gait policy_dir"
+   echo "Usage: train.sh robot gait policy_foldername"
    echo "options:"
    echo "         robot: A1 MC Laikago"
    echo "         gait: trot pace"
-   echo "         policy_dir: directory where the policy would be saved to"
+   echo "         policy_foldername: folder where the policy would be saved to"
    echo "example: train MC trot trot01"
 else
    Robot=$1
    Gait=$2
-   Policy_dir=$3
+   Policy_dir=motion_imitation/data/policies/$Robot/$3
+   if [ -d $Policy_dir ]; then
+      echo "Dir $Policy_dir exists"
+   else
+      mkdir $Policy_dir
+      mpiexec -n 8 python3 motion_imitation/run.py --mode train --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$Gait.txt --output_dir $Policy_dir --int_save_freq 10000000
+   fi
 fi
-mkdir motion_imitation/data/policies/$Robot/$Policy_dir
-python3 motion_imitation/run.py --mode train --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$Gait.txt --output_dir motion_imitation/data/policies/$Robot/$Policy_dir --int_save_freq 10000000 --visualize
