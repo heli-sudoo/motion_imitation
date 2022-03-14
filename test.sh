0de2f7b439960056e02d07837f16ee6da0f27b66
He Li <hli25@nd.edu>
2022-03-14 15:44:33 -0400

    modify cmd input argument for policy file

--- test.sh
+++ test.sh
@@ -5,12 +5,12 @@ if [ $# -eq 0 ]; then
    echo "         mode: test rollout"
    echo "         robot: A1 MC Laikago"
    echo "         gait: trot pace"
-   echo "         policy: gait_num"
+   echo "         policy: policy fille under motion_imitation/data/policies"
 else
    Mode=$1
    Robot=$2
    Gait=$3
    Policy=$4
-   python3 motion_imitation/run.py --mode $Mode --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$Gait.txt --model_file motion_imitation/data/policies/$Robot/$Policy.zip --visualize
+   python3 motion_imitation/run.py --mode $Mode --robot $Robot --motion_file motion_imitation/data/motions/$Robot/$Gait.txt --model_file motion_imitation/data/policies/$Policy.zip --visualize
 fi
 
